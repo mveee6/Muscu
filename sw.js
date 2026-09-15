428d00698d665f8535e0c4ddb84c34f6b4e8f109
mveee6 <mvskonthetrack@gmail.com>
2026-09-15 17:57:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'muscu-v2';
+const CACHE = 'muscu-v3';
 const FILES = ['./', './index.html', './manifest.json', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
